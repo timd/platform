2d93df351ed806df4435e998b3ae598986576d75
Tim Duckett <tim@duckett.de>
2017-03-11 15:56:27 +0100

    Switched to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170311123927) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
