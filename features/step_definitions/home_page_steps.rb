8a4fc39d80a438fe8a37762bdbb4151bcdee5f86
Tim Duckett <tim@duckett.de>
2017-03-11 15:35:26 +0100

    Tweak to feature

--- features/step_definitions/home_page_steps.rb
+++ features/step_definitions/home_page_steps.rb
@@ -6,6 +6,8 @@ When(/^I am on the homepage$/) do
   visit root_path
 end
 
-Then(/^I should see the 'Hello, World!' post$/) do
-  expect(page).to have_content("Hello, world...")
+Then(/^I should see the '(.*?)' post$/) do |title|
+  @post = Post.find_by_title(title)
+  expect(page).to have_content(@post.title)
+  expect(page).to have_content(@post.content)
 end
